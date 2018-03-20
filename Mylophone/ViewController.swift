dfdbb316bbb14c93b0bf2421476e1840e41eb5eb
edelafuente22 <ofthefountain22@gmail.com>
2018-03-20 14:52:41 -0700

    Initial Commit

---
+++ Mylophone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mylophone
+//
+//  Created by Edward de la Fuente on 3/20/18.
+//  Copyright © 2018 Claitco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
